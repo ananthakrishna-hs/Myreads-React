ef4d55551692d54254c864e1e424ded3ce7df710
Ananthakrishna H S <h.s.ananthakrishna@gmail.com>
2018-11-05 16:50:14 +0530

    refactor: Made Shelf component a SFC
    Stateless Functional Componenets are used whenever the componenet does
    only rendering UI and doesnot contain state or any other properties.
    This increases understandability, increases performance and decreases
    code.

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Book from './Book'
 
-class Shelf extends Component {
-  render() {
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.shelfName}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {this.props.bookArray.map(book => (
-              <Book key={book.id} bookObject={book} triggerUpdate={this.props.triggerUpdate} />//<Book key={book.id} {...book} />
-            ))}
-          </ol>
-        </div>
+const Shelf = (props) => {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{props.shelfName}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {props.bookArray.map(book => (
+            <Book key={book.id} bookObject={book} triggerUpdate={props.triggerUpdate} />//<Book key={book.id} {...book} />
+          ))}
+        </ol>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Shelf
