6c01d9f9ab216884187336c69478653a17316bc8
Ananthakrishna H S <h.s.ananthakrishna@gmail.com>
2018-11-09 10:13:22 +0530

    fix: Shelf information in search page
    The search page results did not show the shelf to which the book belongs
    to. After rectyfying it, it also dynamically updates the shelf if there
    is any change.

--- src/Search.js
+++ src/Search.js
@@ -6,16 +6,50 @@ import * as BooksAPI from './BooksAPI'
 class Search extends Component {
   state = {
     books: [],
+    shelfBooks: [],
     search: ''
+  }
+
+  componentDidMount() {
+    this.getShelfBooks();
+  }
+
+  getShelfBooks() {
+    BooksAPI.getAll().then(function(response) {
+      let shelfArray = [];
+      for(let i = 0;i < response.length;i++) {
+        let shelfObject = {
+          id: response[i].id,
+          shelf: response[i].shelf
+        };
+        shelfArray.push(shelfObject);
+      }
+      this.setState({shelfBooks: shelfArray});
+    }.bind(this));
   }
 
   searchFunction = (event) => {
     const searchExp = event.target.value.trim();
+    this.searchBooks(searchExp);
+  }
+
+  searchBooks(searchExp) {
     this.setState({search: searchExp});
     if(searchExp)
       BooksAPI.search(searchExp).then(function(res) {
-        if(Array.isArray(res)) 
+        if(Array.isArray(res)) {
+          for(let i = 0;i < res.length;i++) {
+            let temp = this.state.shelfBooks.find(function(element) {
+              return element.id === res[i].id;
+            });
+            if(temp)
+              res[i].shelf = temp.shelf;
+            else
+              res[i].shelf = 'none';
+          }
           this.setState({books: res});
+
+        }
         else
           this.setState({books: []});
       }.bind(this));
@@ -24,7 +58,12 @@ class Search extends Component {
   }
 
   updateShelf = (book, shelfValue) => {
-    BooksAPI.update(book, shelfValue);
+    BooksAPI.update(book, shelfValue).then(function(response) {
+      this.setState({books: []});
+      this.getShelfBooks();
+      this.searchBooks(this.state.search);
+      console.log(response);
+    }.bind(this));
   }
 
   render() {
@@ -33,7 +72,7 @@ class Search extends Component {
         <div className="search-books-bar">
           <Link to="/" className="close-search">Close</Link>
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author" onChange={this.searchFunction}/>
+            <input type="text" autoFocus placeholder="Search by title or author" onChange={this.searchFunction}/>
           </div>
         </div>
         <div className="search-books-results">
